e09fc36476eadbfec4ac80eb33b0a10a72103eed
Stephen F. Booth <me@sbooth.org>
2009-02-16 22:27:41 -0800

    Converted to C for speed

--- GetMetadataForFile.c
+++ GetMetadataForFile.c
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (C) 2006 - 2008 Stephen F. Booth <me@sbooth.org>
+ *  Copyright (C) 2006 - 2009 Stephen F. Booth <me@sbooth.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -28,36 +28,36 @@ GetMetadataForFile(void							*thisInterface,
 				   CFStringRef					contentTypeUTI,
 				   CFStringRef					pathToFile)
 {
-	FLAC__Metadata_Chain		*chain						= NULL;
-	FLAC__Metadata_Iterator		*iterator					= NULL;
-	char						*fileSystemRepresentation	= NULL;
-	unsigned					i							= 0;
-
 	
-	chain = FLAC__metadata_chain_new();
-	if(!chain)
-		return FALSE;
+#pragma unused(thisInterface)
+#pragma unused(contentTypeUTI)
 
-	
+	// Convert the path to a format suitable for FLAC
 	CFIndex maxLen = CFStringGetMaximumSizeOfFileSystemRepresentation(pathToFile);
-	fileSystemRepresentation = malloc(maxLen);
-	if(!fileSystemRepresentation)
-		goto cleanup;
+	char fileSystemRepresentation [maxLen];
 
 	if(!CFStringGetFileSystemRepresentation(pathToFile, fileSystemRepresentation, maxLen))
-		goto cleanup;
+		return FALSE;
+
+	FLAC__Metadata_Chain *chain = FLAC__metadata_chain_new();
+	// ENOMEM sux
+	if(!chain)
+		return FALSE;
 	
+	// Parse the FLAC metadata
+	if(!FLAC__metadata_chain_read(chain, fileSystemRepresentation)) {
+		FLAC__metadata_chain_delete(chain);
+		return FALSE;
+	}
 	
-	if(!FLAC__metadata_chain_read(chain, fileSystemRepresentation))
-		goto cleanup;
-	
-	
-	iterator = FLAC__metadata_iterator_new();
-	if(!iterator)
-		goto cleanup;
+	FLAC__Metadata_Iterator *iterator = FLAC__metadata_iterator_new();
+	// Once again, not enough memory
+	if(!iterator) {
+		FLAC__metadata_chain_delete(chain);
+		return FALSE;
+	}
 	
 	FLAC__metadata_iterator_init(iterator, chain);
-
 	
 	do {
 		
@@ -86,58 +86,60 @@ GetMetadataForFile(void							*thisInterface,
 					CFDictionarySetValue(attributes, kMDItemDurationSeconds, duration);
 					CFRelease(duration);
 				}
-
 				break;
 				
-			case FLAC__METADATA_TYPE_VORBIS_COMMENT:					
-
-				for(i = 0; i < block->data.vorbis_comment.num_comments; ++i) {
-					
+			case FLAC__METADATA_TYPE_VORBIS_COMMENT:
+				{
 					char *fieldName = NULL;
 					char *fieldValue = NULL;
 
-					// Ignore malformed comments
-					if(!FLAC__metadata_object_vorbiscomment_entry_to_name_value_pair(block->data.vorbis_comment.comments[i], &fieldName, &fieldValue))
-						continue;
+					unsigned commentIndex;
+					for(commentIndex = 0; commentIndex < block->data.vorbis_comment.num_comments; ++commentIndex) {
+						// Parse a vorbis comment (in the NAME=VALUE format) to a pair of values
+						// Ignore malformed comments
+						if(!FLAC__metadata_object_vorbiscomment_entry_to_name_value_pair(block->data.vorbis_comment.comments[commentIndex], &fieldName, &fieldValue))
+							continue;
+						
+						CFStringRef key = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, fieldName, kCFStringEncodingASCII, kCFAllocatorMalloc);
+						CFStringRef value = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, fieldValue, kCFStringEncodingUTF8, kCFAllocatorMalloc);
+						
+						if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ALBUM"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemAlbum, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ARTIST"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemAuthors, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("COMPOSER"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemComposer, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("GENRE"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemMusicalGenre, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DATE"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemRecordingYear, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DESCRIPTION"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemComment, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("TITLE"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemTitle, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("TRACKNUMBER"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemAudioTrackNumber, value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("TRACKTOTAL"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_trackTotal"), value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("COMPILATION"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_compilation"), value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DISCNUMBER"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_discNumber"), value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DISCTOTAL"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_discTotal"), value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ISRC"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_ISRC"), value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("MCN"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_MCN"), value);
+						else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ENCODER"), kCFCompareCaseInsensitive))
+							CFDictionarySetValue(attributes, kMDItemAudioEncodingApplication, value);
+						
+						CFRelease(key), key = NULL;
+						CFRelease(value), value = NULL;
 
-					CFStringRef key = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, fieldName, kCFStringEncodingASCII, kCFAllocatorMalloc);
-					CFStringRef value = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, fieldValue, kCFStringEncodingUTF8, kCFAllocatorMalloc);
-
-					if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ALBUM"), kCFCompareCaseInsensitive))
-					   CFDictionarySetValue(attributes, kMDItemAlbum, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ARTIST"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemAuthors, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("COMPOSER"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemComposer, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("GENRE"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemMusicalGenre, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DATE"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemRecordingYear, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DESCRIPTION"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemComment, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("TITLE"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemTitle, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("TRACKNUMBER"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemAudioTrackNumber, value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("TRACKTOTAL"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_trackTotal"), value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("COMPILATION"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_compilation"), value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DISCNUMBER"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_discNumber"), value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("DISCTOTAL"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_discTotal"), value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ISRC"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_ISRC"), value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("MCN"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, CFSTR("org_xiph_flac_MCN"), value);
-					else if(kCFCompareEqualTo == CFStringCompare(key, CFSTR("ENCODER"), kCFCompareCaseInsensitive))
-						CFDictionarySetValue(attributes, kMDItemAudioEncodingApplication, value);
-					
-					CFRelease(key);
-					CFRelease(value);
+						fieldName = fieldValue = NULL;
+					}				
 				}
-					
 				break;
 				
 			case FLAC__METADATA_TYPE_PICTURE:						break;
@@ -149,7 +151,7 @@ GetMetadataForFile(void							*thisInterface,
 		}
 	} while(FLAC__metadata_iterator_next(iterator));
 
-	
+	// Tell Spotlight what type of data we indexed
 	CFStringRef soundTypes [1];
 	soundTypes[0] = CFSTR("Sound");
 	
@@ -157,18 +159,12 @@ GetMetadataForFile(void							*thisInterface,
 	CFDictionarySetValue(attributes, kMDItemMediaTypes, mediaTypes);
 	CFRelease(mediaTypes);
 	
-	
-	cleanup:
-
-	if(fileSystemRepresentation)
-		free(fileSystemRepresentation);
-
+	// Clean  up
 	if(iterator)
 	   FLAC__metadata_iterator_delete(iterator);
 
 	if(chain)
 	   FLAC__metadata_chain_delete(chain);
-
 
 	return TRUE;
 }
